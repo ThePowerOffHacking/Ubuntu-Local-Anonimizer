03b9e5d22355dac1ef1941ca7862cf621f955807
The Power Off Hacking <142690061+ThePowerOffHacking@users.noreply.github.com>
2023-08-19 22:50:49 +0200

    Requirements.sh

--- Requirements.sh
+++ Requirements.sh
@@ -2,11 +2,4 @@ echo
 echo "[*] Instalando requisitos"
 echo
 sudo apt install -y net-tools trash-cli macchanger sox figlet ufw alsa-utils kmod network-manager
-Error=$?
-if [[ $Error == "0" ]];; then
-	sleep 0.1
-else
-	echo "Ocurrieron errores"
-	exit 1
-fi				
 
